bb1bed6b0288e608b5477f31e70b41cce1ffe962
Nedgang <nedgang@mailoo.org>
2017-05-15 14:25:34 +0200

    Initial add

---
+++ stat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Explanations
+
+Usage:
+    ./stat.py <data.csv>
+
+Options:
+    -h, --help      Show this screen
+    -v, --version   Show version
+"""
+
+##########
+# IMPORT #
+##########
+import pandas as pd
+
+from docopt import docopt
+
+########
+# MAIN #
+########
+def main(args):
+    data = pd.read_csv(args["<data.csv>"]).groupby("Lot")
+    data.describe().to_csv("stats_"+args["<data.csv>"])
+
+#############
+# FUNCTIONS #
+#############
+
+##########
+# LAUNCH #
+##########
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version="0.1")
+    main(arguments)
