5d010f069432a570840d02957aafea87b0c5ca94
Nedgang <nedgang@mailoo.org>
2017-05-19 09:30:03 +0200

    Change plot type to histogram

--- shape_res.py
+++ shape_res.py
@@ -45,7 +45,7 @@ def res_shape_drawing(args):
     # Draw the graphs
     for res in residuts:
         print("Drawing the " + res + " graph")
-        dt[res].plot(kind='density')
+        dt[res].plot(kind='hist')
         # Hide Y axis
         # frame = plt.gca()
         # frame.axes.get_yaxis().set_visible(False)
